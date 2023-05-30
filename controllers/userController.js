73ccb2e018de1f4c59c4e04032d6ac750cb4b069
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-05-30 06:24:48 -0400

    Started working on the routes and have been working on the controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,19 @@
-const {Network} = require('../models')
+const {User, Thought} = require('../models')
 
 module.exports = {
-    
+    getUsers(req, res) {
+        User.find()
+        .then((users) => res.json(users))
+        .catch((err)=> res.status(500).json(err))
+    },
+    getSingleUser(req, res) {
+        User.findOne({_id: req.params.userId})
+        .then((userId) => res.json(userId))
+        .catch((err)=> res.status(500).json(err))
+    },
+    createNewUser(req, res) {
+        User.create(req.body)
+        .then((newUser) => res.json(newUser))
+        .catch((err)=> res.status(500).json(err))
+    }
 }
