08a8f36d6edfee6247226713400e72496004dafd
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-05-30 06:09:13 -0400

    Finished up the models

---
+++ controllers/userController.js
@@ -0,0 +1,5 @@
+const {Network} = require('../models')
+
+module.exports = {
+    
+}
--- models/Thought.js
+++ models/Thought.js
@@ -5,7 +5,48 @@ const thoughtSchema = new Schema(
         thoughtText: {
             type: String,
             required: true,
-            length: 
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    }, {
+        virtual: {
+            reactionCount:
+            {get(){
+                return this.reactions.length
+            }
+        }
         }
     }
 )
+const reactionSchema = new Schema({
+    reactionId: {
+        type: ObjectId,
+        default: new ObjectId
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+const Thought = mongoose.model('Thought', thoughtSchema)
+
+module.exports = Thought
